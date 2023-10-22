3d486964e0d7fd7ea426586445e16bd6b125e32c
sebastian_lagos <info.trabajo.seba@gmail.com>
2023-10-22 16:24:09 -0300

    GTM

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,7 +2,6 @@ import './globals.css'
 import { Inter } from 'next/font/google'
 import Footer from './components/footer'
 import Slogan from './components/Slogan'
-import Script from 'next/script'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,18 +14,16 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <Script src='https://www.googletagmanager.com/gtag/js?id=AW-11376305852' />
-        <Script id="google-analytics">
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);} 
-          gtag('js', new Date()); 
-          gtag('config', 'AW-11376305852');
-        `}
-        </Script>
+        <script dangerouslySetInnerHTML={{__html:`window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'AW-11376305852');`}} />
       </head>
       <Slogan />
       <body className={inter.className}>
+      <iframe
+        src={`https://www.googletagmanager.com/gtag/js?id=AW-11376305852`}
+        height="0"
+        width="0"
+        style={{ display: "none", visibility: "hidden" }}
+      ></iframe>
         {children}
         <Footer />
       </body>
