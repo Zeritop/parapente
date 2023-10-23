e841e417d0894f1c874b06140cf8670807dbcbe8
sebastian_lagos <info.trabajo.seba@gmail.com>
2023-10-22 23:03:09 -0300

    Otro GTM

--- src/app/layout.js
+++ src/app/layout.js
@@ -14,16 +14,19 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
+        <script async src='https://www.googletagmanager.com/gtag/js?id=AW-11376305852'></script>
         <script dangerouslySetInnerHTML={{__html:`window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'AW-11376305852');`}} />
       </head>
       <Slogan />
       <body className={inter.className}>
-      <iframe
-        src={`https://www.googletagmanager.com/gtag/js?id=AW-11376305852`}
-        height="0"
-        width="0"
-        style={{ display: "none", visibility: "hidden" }}
-      ></iframe>
+      <noscript>
+        <iframe
+          src={`https://www.googletagmanager.com/gtag/js?id=AW-11376305852`}
+          height="0"
+          width="0"
+          style={{ display: "none", visibility: "hidden" }}
+        ></iframe>
+      </noscript>
         {children}
         <Footer />
       </body>
