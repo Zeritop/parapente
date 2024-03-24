8a9d7e190cba83d164cf23ead226487de91da0e9
sebastian_lagos <info.trabajo.seba@gmail.com>
2024-03-23 21:22:25 -0300

    Nuevo tarifas

--- src/app/components/Tarifas.jsx
+++ src/app/components/Tarifas.jsx
@@ -11,25 +11,31 @@ const Tarifas = () => {
   }, [])
   return (
     <div className='container_tarifas' id='tarifas'>
-        <h2 style={{textAlign: 'center', fontSize: '40px'}}>Tarifas producto - paquetes</h2>
+      <div style={{width: '100%', height: '100%'}}>
+        {/* <h2 style={{textAlign: 'center', fontSize: '40px'}}>Tarifas producto - paquetes</h2> */}
         <Image
-            src={'/images/new_tarifas.png'}
+            src={'/images/img_tarifas.png'}
+            layout='responsive'
             width={1000}
             height={600}
             alt=''
         />
+      </div>
         <div className='containerVideoTarifas'>
-            <ReactPlayer url={'/videos/Opción gopro.MP4'} controls width='100%' />
-            <ReactPlayer url={'/videos/Opción insta.MP4'} controls width='100%' />
+            <ReactPlayer url={'/videos/video-plano-web.MP4'} controls width='100%' />
+            <ReactPlayer url={'/videos/video360web.MP4'} controls width='100%' />
         </div>
         <br />
-        <h2 style={{textAlign: 'center', fontSize: '40px', marginTop: '1%', marginBottom: '1%'}}>Vuelos Especiales</h2>
-        <Image
-            src={'/images/vuelos_esp.png'}
-            width={1000}
-            height={600}
-            alt=''
-        />
+        <div style={{width: '100%'}}>
+          <h2 style={{textAlign: 'center', fontSize: '40px', marginTop: '1%', marginBottom: '1%'}}>Vuelos Especiales</h2>
+          <Image
+              src={'/images/vuelos_esp.png'}
+              layout='responsive'
+              width={1000}
+              height={600}
+              alt=''
+          />
+        </div>
     </div>
   )
 }
